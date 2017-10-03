ba57d131151382ce40e23fafdca03964577f2d03
TerryRios <terry_7_98@hotmail.com>
2017-10-03 17:22:11 -0500

    2

---
+++ p.e/Assets/Editor/MapEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileMap))]
+public class MapEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		
+		TileMap map = target as TileMap;
+
+		if (DrawDefaultInspector()) {
+			map.GenerateMap ();
+		}
+		if (GUILayout.Button("Generate Map")) {
+			map.GenerateMap ();
+		}
+	}
+}
---
+++ p.e/Assets/script/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip mainTheme;
+	public AudioClip menuTheme;
+
+	void Start(){
+		AudioManager.instance.PlayMusic (menuTheme, 2);
+		//para colocar audio ""public AudioClip"" 
+		//y luego colocar AudioManager.instance.playSound/music(""audio"",transform.position)en donde se ejecute;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
